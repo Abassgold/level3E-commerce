dfdacd724dfb1a1d07b641b407a8c79fec3ccd3f
Abassgold <Abasskola10@yahoo.com>
2023-11-06 12:27:05 +0100

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
 
 function App() {
   return (
     <div>
+      <Navbar/>
+      <Routes>
+        <Route index element={<Home/>}/>
+      </Routes>
     </div>
   )
 }
